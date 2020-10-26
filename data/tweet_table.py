f58523bfff7eb952ab1e5d7832f52da171bcfa31
Quinton Yong <quintillion.y@gmail.com>
2020-10-25 20:56:19 -0700

    parsing a tweet obtained by query

--- data/tweet_table.py
+++ data/tweet_table.py
@@ -1,11 +1,12 @@
 import pymongo
 import tweepy
+import inspect
 
 client = pymongo.MongoClient("mongodb+srv://Quintillion:TzjTGcE5I6Bu7P9e@twitterdata.wkwqp.mongodb.net/TwitterData?retryWrites=true&w=majority")
 db = client["TwitterData"]
 collection = db["Tweets"]
 
-""" row = {"tweetid":1, "userid":1, "content":"quinton", "time":"8:00", "geotag":"somewhere", "party":"R", "sentiment":"N"}
+""" row = {"tweetid":1, "userid":1, "text":"quinton", "created_at":"8:00", "geotag":"somewhere", "party":"R", "sentiment":"N"}
 
 x = collection.insert_one(row)
 print(x.inserted_id) """
@@ -18,5 +19,15 @@ api = tweepy.API(auth)
 
 query = '#biden OR #biden2020 OR #trump OR #trump2020'
 
+# useful members in tweet object: created_at, id, text, 
+
 for tweet in tweepy.Cursor(api.search, q=query, count = 1, result_type = "mixed").items(1):
+    #print(inspect.getmembers(tweet))
     print(tweet.text)
+    print(tweet.id)
+    print(tweet.user.id)
+    print(tweet.created_at)
+    print([hashtag['text'] for hashtag in tweet.entities['hashtags']]) # this is a list of all the hashtags in the tweet. 
+                                                                       # We can get party and sentiment by creating a dict of hashtags to "(D or R), (N, +, -)"
+
+
