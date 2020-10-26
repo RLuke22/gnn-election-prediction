aacd816e92cc1f42e36836a28e96a2f0e6f3b13f
Luke Rowe <lukerowe1222@gmail.com>
2020-10-26 13:38:23 -0600

    tested tweepy code

--- data/tweet_table.py
+++ data/tweet_table.py
@@ -11,23 +11,64 @@ collection = db["Tweets"]
 x = collection.insert_one(row)
 print(x.inserted_id) """
 
-consumer_key_Q = "DT3XJxLbG9gSAExLTnw8Ya8wy"
-consumer_secret_Q = "9zhg6sRhqaBBgzYyBuoaohPH6WLpYEMWkg3S0koAhr50dfZQ1q"
+# Luke's keys
+consumer_key_Q = "JZNzXeOD8VMDCroiKFXsXwAdg"
+consumer_secret_Q = "O58HHCidMQ8bogw4ofs8hr50V45aYxAG2i9vvqfZBFPXI3zKjM"
 
 auth = tweepy.AppAuthHandler(consumer_key_Q, consumer_secret_Q)
 api = tweepy.API(auth)
 
-query = '#biden OR #biden2020 OR #trump OR #trump2020'
+hashtags_dem = ['#biden', 
+                '#biden2020', 
+                '#bidenharris', 
+                '#bidenharris2020', 
+                '#joebiden', 
+                '#kamalaharris',
+                '#votebidenharris2020',
+                '#theresistance',
+                '#resist',
+                '#votebluetosaveamerica',
+                '#votebiden',
+                '#voteblue',
+                '#bidenharrislandslide2020',
+                '#sleepyjoe',
+                '#neverbiden']
 
-# useful members in tweet object: created_at, id, text, 
+hashtags_rep = ['#trump',
+                '#trump2020',
+                '#trumppence',
+                '#trumppence2020',
+                '#donaldtrump',
+                '#mikepence',
+                '#maga',
+                '#maga2020',
+                '#kag',
+                '#voteredtosaveamerica',
+                '#votetrump',
+                '#votered',
+                '#trump2020landslide',
+                '#votehimout',
+                '#dumptrump']
 
-for tweet in tweepy.Cursor(api.search, q=query, count = 1, result_type = "mixed").items(1):
-    #print(inspect.getmembers(tweet))
-    print(tweet.text)
-    print(tweet.id)
-    print(tweet.user.id)
-    print(tweet.created_at)
-    print([hashtag['text'] for hashtag in tweet.entities['hashtags']]) # this is a list of all the hashtags in the tweet. 
-                                                                       # We can get party and sentiment by creating a dict of hashtags to "(D or R), (N, +, -)"
+hashtags = hashtags_dem + hashtags_rep
+# currently query is 471 characters
+query = ' OR '.join(hashtags)
 
+tweet_count = 5
+for tweet_info in tweepy.Cursor(api.search, q=query, count = tweet_count, result_type = "recent", tweet_mode="extended").items(tweet_count):
+    if 'retweeted_status' in dir(tweet_info):
+        tweet=tweet_info.retweeted_status.full_text
+    else:
+        tweet=tweet_info.full_text
+
+    print("--------------------------------------------------------------------")
+    print(tweet)
+    print(tweet_info.id)
+    print(tweet_info.user.id)
+    print(tweet_info.created_at)
+    # We can get party and sentiment by creating a dict of hashtags to "(D or R), (N, +, -)"
+    print([hashtag['text'] for hashtag in tweet_info.entities['hashtags']])
+    print()
+
+                                                            
 
