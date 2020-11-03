2af1deb92ea2a1a2ce7526d0dc9b9b26cc410104
Quinton Yong <quintillion.y@gmail.com>
2020-11-02 16:18:36 -0800

    added coloring of states

--- visual/results_map.py
+++ visual/results_map.py
@@ -1,23 +1,26 @@
 import matplotlib.pyplot as plt 
 import geopandas
 
-""" class ElectionResultsMap():
+class ElectionResultsMap():
     def __init__(self):
         super(ElectionResultsMap, self).__init__()
 
         self.states = geopandas.read_file('usa-states-census-2014.shp')
+        self.states = self.states.to_crs("EPSG:3395")
         
     def show(self):
-        print("show")
-        self.states.plot()
+        fig = plt.figure(1, figsize=(12, 12))
+        ax = fig.add_subplot()
+
+        self.states.boundary.plot(ax=ax, color='Black', linewidth=0.4)
+        self.states.plot(ax=ax, color='whitesmoke', figsize=(12, 12))
+
+        self.states[self.states['NAME']=='Texas'].plot(ax = ax, color='tab:red')
+        self.states[self.states['NAME']=='Ohio'].plot(ax = ax, color='tab:blue')
+        
         plt.savefig('map.png')
         plt.show()
 
 if __name__ == '__main__':
     map = ElectionResultsMap()
-    map.show() """
-
-states = geopandas.read_file('usa-states-census-2014.shp')
-states.plot()
-plt.savefig('map.png')
-plt.show()
+    map.show()
